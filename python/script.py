4f8d5934f84c0f921743dfa7dd582cbaca3452b6
vnn1489 <vnn1489@outlook.com>
2023-09-27 06:14:14 +0700

    20230927_0614

--- python/script.py
+++ python/script.py
@@ -13,31 +13,28 @@ response = requests.get(url, headers=headers)
 
 if response.status_code == 200:
     repositories = response.json()
+    git_branch = "git branch -r | grep -v '\->' | sed 's,\x1B\[[0-9;]*[a-zA-Z],,g' | while read remote; do git branch --track \"${remote#origin/}\" \"$remote\"; done"
+    git_fetch = "git fetch --all"
+    git_pull = "git pull --all"
 
     print(f"STARTING CLONE ALL REPOSITORIES")
     for repository in repositories:
-        
+        print(f"CLONING {repository['html_url']}")
         try:
-            print(f"CLONING {repository['html_url']}")
             git_clone = f"git clone {repository['html_url']}"
-            run_git_clone = subprocess.run(git_clone, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            subprocess.run(git_clone, shell=True, check=True)
             os.chdir(f"{os.getcwd()}/{repository['name']}")
 
-            git_branch_command = "git branch -r | grep -v '\->' | sed 's,\x1B\[[0-9;]*[a-zA-Z],,g' | while read remote; do git branch --track \"${remote#origin/}\" \"$remote\"; done"
-            git_fetch_command = "git fetch --all"
-            git_pull_command = "git pull --all"
-
+            # CHUA HIEU DOAN TRY EXCEPT NAY LAI KHIEN CHO CAC DIRECTORY KHOONG BI LONG VAO NHAU
             try:
-                # Run the Git commands one by one
-                subprocess.run(git_branch_command, shell=True, check=True)
-                subprocess.run(git_fetch_command, shell=True, check=True)
-                subprocess.run(git_pull_command, shell=True, check=True)
+                subprocess.run(git_branch, shell=True, check=True)
+                subprocess.run(git_fetch, shell=True, check=True)
+                subprocess.run(git_pull, shell=True, check=True)
             except subprocess.CalledProcessError as e:
                 print(f"Error: {e}")
 
             previous_directory = os.path.dirname(os.getcwd())
             os.chdir(previous_directory)
-           
         except Exception as e:
             print(f"AN ERROR OCCURRED: {str(e)}")
     print(f"CLONED ALL REPOSITORIES")

b38172cc4d6521979209cdfbf6a8fc87cbde22b8
vnn1489 <vnn1489@outlook.com>
2023-09-27 01:56:50 +0700

    20230927_0156

--- python/script.py
+++ python/script.py
@@ -4,7 +4,6 @@ import os
 
 username = 'vnn1489'
 url = f"https://api.github.com/users/{username}/repos"
-current_directory = os.getcwd()
 
 # Set the desired headers for the request
 headers = {"Accept": "application/vnd.github.v3+json"}
@@ -22,24 +21,23 @@ if response.status_code == 200:
             print(f"CLONING {repository['html_url']}")
             git_clone = f"git clone {repository['html_url']}"
             run_git_clone = subprocess.run(git_clone, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            os.chdir(f"{os.getcwd()}/{repository['name']}")
 
-            os.chdir(f"cd \"{current_directory}/{repository['name']}\"")
-        
-            # run_cd = subprocess.run(cd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            git_branch_command = "git branch -r | grep -v '\->' | sed 's,\x1B\[[0-9;]*[a-zA-Z],,g' | while read remote; do git branch --track \"${remote#origin/}\" \"$remote\"; done"
+            git_fetch_command = "git fetch --all"
+            git_pull_command = "git pull --all"
 
-            git_branch = "git branch -r | grep -v \"\->\" | sed 's,\x1B\[[0-9;]*[a-zA-Z],,g' | while read remote; do git branch --track '${remote#origin/}' '$remote'; done"
-            run_git_branch = subprocess.run(git_branch, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            try:
+                # Run the Git commands one by one
+                subprocess.run(git_branch_command, shell=True, check=True)
+                subprocess.run(git_fetch_command, shell=True, check=True)
+                subprocess.run(git_pull_command, shell=True, check=True)
+            except subprocess.CalledProcessError as e:
+                print(f"Error: {e}")
 
-            git_fetch = "git fetch --all"
-            run_git_fetch = subprocess.run(git_fetch, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-            
-            git_pull = "git pull --all"
-            run_git_pull = subprocess.run(git_pull, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-
-            os.chdir(f"cd {current_directory}")
-            # cd = f"cd {current_directory}"
-            # run_cd = subprocess.run(cd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-
+            previous_directory = os.path.dirname(os.getcwd())
+            os.chdir(previous_directory)
+           
         except Exception as e:
             print(f"AN ERROR OCCURRED: {str(e)}")
     print(f"CLONED ALL REPOSITORIES")
